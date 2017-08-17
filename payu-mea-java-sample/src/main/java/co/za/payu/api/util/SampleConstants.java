4ad222cf1cec45c71200127e8032dbe7d7ba1aa7
Kenneth Onah <kenneth@netcraft-devops.com>
2017-08-17 21:32:49 +0200

    sample constants and result printer

---
+++ payu-mea-java-sample/src/main/java/co/za/payu/api/util/SampleConstants.java
@@ -0,0 +1,11 @@
+package co.za.payu.api.util;
+
+/**
+ * Created by kenny on 8/17/17.
+ */
+public class SampleConstants {
+    public static final String apiUsername = "AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS";
+    public static final String apiPassword = "EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL";
+    public static final String safeKey = "EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL";
+    public static final String mode = "sandbox";
+}
